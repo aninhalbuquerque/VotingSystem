f7b7888a7b8da5862241f14892c069b87c3b7a27
Ana Albuquerque <51363891+aninhalbuquerque@users.noreply.github.com>
2020-11-07 12:29:58 -0300

    Update server.py
    add arq

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 from protocol import *
-
 sock = Protocol()
 
 sock.open_server(10000)
 
-while True:
+
+while sock.cond:
     sock.server_wait_connection()
+    if sock.cond == False: 
+        print("Limite de usuarios simultaneos excedido, o servidor ira fechar")
+        arquivo = open("usuarios.txt", "w")
+        for user in sock.users:
+            arquivo.writelines(user + " " + sock.users[user] + "\n")
+        arquivo.close()
