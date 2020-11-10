12bbea409c1eab4eed86374e07720236b42190bf
Ana Albuquerque <51363891+aninhalbuquerque@users.noreply.github.com>
2020-11-09 23:11:07 -0300

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,44 @@ sock = Protocol()
 
 sock.open_server(10000)
 
-
 while sock.cond:
     sock.server_wait_connection()
+    
+    #print(choice)
+    ret = False 
+    while not ret:
+        choice = sock.recv_hash(sock.client)
+        if choice == '1':
+            ok = False
+            while not ok:
+                ok = sock.user_login_server()
+            
+            ret = True
+        else:
+            ok = False
+            while not ok:
+                ok = sock.cadastro_server()
+    
+    ret = False 
+    while not ret:
+        choice = sock.recv_hash(sock.client)
+        if choice == '1':
+            sock.send_voting_sections()
+            sock.send_options()
+            sock.server_vote()
+    
+        elif choice == '2':
+            sock.send_voting_sections()
+            sock.server_results()
+
+        elif choice == '3':
+            ok = sock.server_create_session()
+            while not ok:
+                ok = sock.server_create_session()
+        else:
+            sock.close_connection(sock.client)
+            ret = True
+
     if sock.cond == False: 
         print("Limite de usuarios simultaneos excedido, o servidor ira fechar")
         arquivo = open("usuarios.txt", "w")
